2437ae55845c286cec8dc7772c8ad62b989dd70a
proverasamuel-sys <provera.samuel@multitel.co.ao>
2025-06-27 15:24:11 +0100

    feat: service de recursos adicionados

--- src/app/components/benefits/benefits.component.ts
+++ src/app/components/benefits/benefits.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit} from '@angular/core';
+import { Recurso, RecursosService } from '../../services/recursos.service';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-benefits',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './benefits.component.html',
   styleUrl: './benefits.component.css'
 })
 export class BenefitsComponent {
-
+   @Input() paginaId!: number; // recebe a página como input
+    recursos: Recurso[] = [];
+  constructor(private recursosService: RecursosService) {}
+  ngOnInit(): void {
+    if (this.paginaId) {
+      this.recursosService.getRecursosByPagina(this.paginaId).subscribe({
+        next: (data) => {
+          this.recursos = data;
+        },
+        error: (err) => console.error('Erro ao carregar recursos:', err)
+      });
+    }
+  }
 }
--- src/app/services/recursos.service.ts
+++ src/app/services/recursos.service.ts
@@ -2,28 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-
-
-export interface Hero {
+export interface Recurso {
   id: number;
   titulo: string;
-  descricao: string;
-  texto_botao?: string;
   imagem: string;
 }
-
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
+export class RecursosService {
 
   private API_URL = 'http://127.0.0.1:8000/api'; 
 
   constructor(private http: HttpClient) {}
 
-  getHeroesByPagina(paginaId: number): Observable<Hero[]> {
-    return this.http.get<{ status: boolean, data: Hero[] }>(`${this.API_URL}/paginas/${paginaId}/heroes`)
+  getRecursosByPagina(paginaId: number): Observable<Recurso[]> {
+    return this.http.get<{ status: boolean, data: Recurso[] }>(`${this.API_URL}/paginas/${paginaId}/recursos`)
       .pipe(map(response => response.data));
   }
 }
