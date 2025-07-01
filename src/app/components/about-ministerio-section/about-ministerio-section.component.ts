38a7c9965660190e7258426f8823ad4f0d791785
proverasamuel-sys <provera.samuel@multitel.co.ao>
2025-07-01 11:20:33 +0100

    Feat: Heroes com as paginas correspindentes

--- src/app/components/about-ministerio-section/about-ministerio-section.component.ts
+++ src/app/components/about-ministerio-section/about-ministerio-section.component.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-ministerio-section',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './about-ministerio-section.component.html',
   styleUrl: './about-ministerio-section.component.css'
 })
 export class AboutMinisterioSectionComponent {
+ 
 
+atribuicoesExpanded = false;
+
+  toggleAtribuicoes() {
+    this.atribuicoesExpanded = !this.atribuicoesExpanded;
+  }
 }
